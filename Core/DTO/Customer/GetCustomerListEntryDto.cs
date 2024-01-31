dd951c70027ce95e7ecbe093e5b13cc9f955e455
vezixig <vezixig@googlemail.com>
2024-01-31 20:16:58 +0100

    Full adress for customer list

--- Core/DTO/Customer/GetCustomerListEntryDto.cs
+++ Core/DTO/Customer/GetCustomerListEntryDto.cs
@@ -9,18 +9,34 @@ public class GetCustomerListEntryDto
     /// <inheritdoc cref="Customer.AssociatedEmployeeId" />
     public Guid AssociatedEmployeeId { get; set; }
 
+    /// <summary>Gets or sets the name of the associated employee.</summary>
     public string AssociatedEmployeeName { get; set; } = "";
 
+    /// <summary>Gets or sets the city of the customer.</summary>
     public string City { get; set; } = "";
 
+    /// <inheritdoc cref="Customer.FirstName" />
     public string FirstName { get; set; } = "";
 
+    /// <inheritdoc cref="Customer.Id" />
     [Key]
     public Guid Id { get; set; }
 
+    /// <inheritdoc cref="Customer.LastName" />
     public string LastName { get; set; } = "";
 
+    /// <inheritdoc cref="Customer.Latitude" />
+    public decimal Latitude { get; set; }
+
+    /// <inheritdoc cref="Customer.Longitude" />
+    public decimal Longitude { get; set; }
+
+    /// <inheritdoc cref="Customer.Phone" />
     public string Phone { get; set; } = "";
 
+    /// <inheritdoc cref="Customer.Street" />
+    public string Street { get; set; } = "";
+
+    /// <inheritdoc cref="Customer.ZipCode" />
     public string ZipCode { get; set; } = "";
 }
