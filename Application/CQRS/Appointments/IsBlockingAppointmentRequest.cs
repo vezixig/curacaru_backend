09863f74195119502bfc0051d52cf8b8727c2888
vezixig <vezixig@googlemail.com>
2024-04-09 19:25:36 +0200

    Endpoint to check if appointment would overlap other appointment

---
+++ Application/CQRS/Appointments/IsBlockingAppointmentRequest.cs
@@ -0,0 +1,41 @@
+﻿namespace Curacaru.Backend.Application.CQRS.Appointments;
+
+using Infrastructure.Repositories;
+using MediatR;
+
+/// <summary>Checks if an appointment would block another appointment.</summary>
+/// <param name="companyId">The company id.</param>
+/// <param name="employeeId">The employee id.</param>
+/// <param name="date">The date of the appointment.</param>
+/// <param name="start">The start time of the appointment.</param>
+/// <param name="end">The end time of the appointment.</param>
+/// <param name="appointmentId">The id of appointment if it already exists to ignore itself.</param>
+public class IsBlockingAppointmentRequest(
+    Guid companyId,
+    Guid employeeId,
+    DateOnly date,
+    TimeOnly start,
+    TimeOnly end,
+    Guid? appointmentId) : IRequest<bool>
+{
+    public Guid? AppointmentId { get; } = appointmentId;
+
+    public Guid CompanyId { get; } = companyId;
+
+    public DateOnly Date { get; } = date;
+
+    public Guid EmployeeId { get; } = employeeId;
+
+    public TimeOnly End { get; } = end;
+
+    public TimeOnly Start { get; } = start;
+}
+
+public class IsBlockingAppointmentRequestHandler(IAppointmentRepository appointmentRepository) : IRequestHandler<IsBlockingAppointmentRequest, bool>
+{
+    public async Task<bool> Handle(IsBlockingAppointmentRequest request, CancellationToken cancellationToken)
+    {
+        var appointments = await appointmentRepository.GetAppointmentsAsync(request.CompanyId, request.Date, request.Date, request.EmployeeId, null);
+        return appointments.Exists(a => a.Id != request.AppointmentId && request.Start < a.TimeEnd && request.End > a.TimeStart);
+    }
+}
---
+++ Sarimon/Endpoints/AppointmentEndpoints.cs
@@ -0,0 +1,38 @@
+﻿namespace Curacaru.Backend.Endpoints;
+
+using System.Security.Claims;
+using Application.CQRS.Appointments;
+using Core.Enums;
+using MediatR;
+
+public class AppointmentEndpoints : EndpointsBase, IEndpoints
+{
+    public void MapEndpoints(WebApplication app)
+    {
+        var deploymentReportEndpoints = app.MapGroup("/appointments");
+
+        MapGetIsBlockingAppointment(deploymentReportEndpoints);
+    }
+
+    private void MapGetIsBlockingAppointment(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet(
+                "/is-blocking",
+                async (
+                    IMediator Mediator,
+                    ClaimsPrincipal principal,
+                    Guid employeeId,
+                    DateOnly date,
+                    TimeOnly start,
+                    TimeOnly end,
+                    Guid? appointmentId) => await Mediator.Send(
+                    new IsBlockingAppointmentRequest(GetCompanyId(principal), employeeId, date, start, end, appointmentId)))
+            .RequireAuthorization(Policy.Company)
+            .WithOpenApi(
+                generatedOperation =>
+                {
+                    generatedOperation.Description = "Checks if an appointment would block another appointment.";
+                    return generatedOperation;
+                });
+    }
+}
