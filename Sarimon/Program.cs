117d56aac94faffaec106d223e3f68a5686d71d1
vezixig <vezixig@googlemail.com>
2024-04-13 18:44:09 +0200

    Serilog with Loki

--- Sarimon/Program.cs
+++ Sarimon/Program.cs
@@ -3,6 +3,7 @@ using Curacaru.Backend.Infrastructure;
 using Curacaru.Backend.Middleware;
 using Curacaru.Backend.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,58 +14,77 @@ builder.Services.AddMemoryCache();
 builder.Services.AddSingleton<ExceptionHandlerMiddleware>();
 builder.Services.AddSingleton<CompanyMiddleware>();
 
-// Add Cors
-builder.Services.AddCors(
-    options =>
-    {
-        options.AddPolicy(
-            "local",
-            policy =>
-            {
-                policy
-                    .WithOrigins("http://localhost:4200")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-            });
-    });
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+    .Build();
 
-// Add Auth
-builder.Services.AddAuthentication(
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
+{
+    // Add Cors
+    builder.Services.AddSerilog();
+    builder.Services.AddHttpLogging(o => { });
+    builder.Services.AddCors(
         options =>
         {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-    .AddJwtBearer(
-        options =>
-        {
-            options.Authority = Environment.GetEnvironmentVariable("IDENTITY_AUTHORITY");
-            options.Audience = Environment.GetEnvironmentVariable("IDENTITY_AUDIENCE");
+            options.AddPolicy(
+                "local",
+                policy =>
+                {
+                    policy
+                        .WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
         });
 
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("Company", policy => { policy.RequireClaim("CompanyId"); })
-    .AddPolicy("Manager", policy => { policy.RequireClaim("Manager"); });
+    // Add Auth
+    builder.Services.AddAuthentication(
+            options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+        .AddJwtBearer(
+            options =>
+            {
+                options.Authority = Environment.GetEnvironmentVariable("IDENTITY_AUTHORITY");
+                options.Audience = Environment.GetEnvironmentVariable("IDENTITY_AUDIENCE");
+            });
 
-// Add app layers
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure();
+    builder.Services.AddAuthorizationBuilder()
+        .AddPolicy("Company", policy => { policy.RequireClaim("CompanyId"); })
+        .AddPolicy("Manager", policy => { policy.RequireClaim("Manager"); });
 
-var app = builder.Build();
+    // Add app layers
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseCors("local");
+    var app = builder.Build();
+    app.UseHttpLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseCors("local");
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseMiddleware<ExceptionHandlerMiddleware>();
+    app.UseMiddleware<CompanyMiddleware>();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.RegisterEndpointsFromAssembly();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseMiddleware<ExceptionHandlerMiddleware>();
-app.UseMiddleware<CompanyMiddleware>();
-app.UseAuthorization();
-app.MapControllers();
-app.RegisterEndpointsFromAssembly();
-
-app.Run();
+catch (Exception ex) { Log.Fatal(ex, "Application terminated unexpectedly"); }
+finally { Log.CloseAndFlush(); }
