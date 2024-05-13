c0d92a9d995b223b4f862f2c044737b32f756cb7
vezixig <vezixig@googlemail.com>
2024-05-13 20:32:46 +0200

    Paging for time tracker

--- Application/CQRS/TimeTracking/WorkingHoursListRequest.cs
+++ Application/CQRS/TimeTracking/WorkingHoursListRequest.cs
@@ -1,5 +1,6 @@
 ﻿namespace Curacaru.Backend.Application.CQRS.TimeTracking;
 
+using Core.DTO;
 using Core.DTO.TimeTracker;
 using Core.Enums;
 using Core.Models;
@@ -9,9 +10,15 @@ using MediatR;
 public class WorkingHoursListRequest(
     User user,
     int year,
-    int month) : IRequest<List<GetWorkingTimeReportListDto>>
+    int month,
+    int page,
+    int pageSize) : IRequest<PageDto<GetWorkingTimeReportListDto>>
 {
     public int Month { get; } = month;
+
+    public int Page { get; } = page;
+
+    public int PageSize { get; } = pageSize;
 
     public User User { get; } = user;
 
@@ -19,18 +26,25 @@ public class WorkingHoursListRequest(
 }
 
 internal class WorkingHoursListRequestHandler(IWorkingTimeRepository workingTimeRepository, IEmployeeRepository employeeRepository)
-    : IRequestHandler<WorkingHoursListRequest, List<GetWorkingTimeReportListDto>>
+    : IRequestHandler<WorkingHoursListRequest, PageDto<GetWorkingTimeReportListDto>>
 {
-    public async Task<List<GetWorkingTimeReportListDto>> Handle(WorkingHoursListRequest request, CancellationToken cancellationToken)
+    public async Task<PageDto<GetWorkingTimeReportListDto>> Handle(WorkingHoursListRequest request, CancellationToken cancellationToken)
     {
         var start = new DateOnly(request.Year, request.Month, 1);
         var end = start.AddMonths(1).AddDays(-1);
 
-        var workedMonths = await workingTimeRepository.GetWorkedMonthsAsync(
+        var workedMonthsCount = await workingTimeRepository.GetWorkedMonthsCountAsync(
             request.User.CompanyId,
             start,
             end,
             request.User.IsManager ? null : request.User.EmployeeId);
+        var workedMonths = await workingTimeRepository.GetWorkedMonthsAsync(
+            request.User.CompanyId,
+            start,
+            end,
+            request.User.IsManager ? null : request.User.EmployeeId,
+            request.Page,
+            request.PageSize);
 
         var reports = await workingTimeRepository.GetWorkingTimeReportsAsync(
             request.User.CompanyId,
@@ -54,6 +68,6 @@ internal class WorkingHoursListRequestHandler(IWorkingTimeRepository workingTime
                 workedMonth.Id = report?.Id;
             });
 
-        return workedMonths.OrderBy(o => o.EmployeeName).ToList();
+        return new(workedMonths, request.Page, (int)Math.Ceiling((double)workedMonthsCount / request.PageSize));
     }
 }
