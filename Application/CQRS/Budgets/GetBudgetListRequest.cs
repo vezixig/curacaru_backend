ef5190470be286fb362bb7146b3eeff00a9a1e65
vezixig <vezixig@googlemail.com>
2024-05-13 20:52:17 +0200

    Pading for budgets

--- Application/CQRS/Budgets/GetBudgetListRequest.cs
+++ Application/CQRS/Budgets/GetBudgetListRequest.cs
@@ -1,14 +1,19 @@
 ﻿namespace Curacaru.Backend.Application.CQRS.Budgets;
 
 using AutoMapper;
+using Core.DTO;
 using Core.DTO.Budget;
 using Infrastructure.Repositories;
 using MediatR;
 
 /// <summary>Request to get the list of current budgets for a company.</summary>
-public class GetBudgetListRequest(Guid companyId) : IRequest<List<GetBudgetListEntryDto>>
+public class GetBudgetListRequest(Guid companyId, int page, int pageSize) : IRequest<PageDto<GetBudgetListEntryDto>>
 {
     public Guid CompanyId { get; } = companyId;
+
+    public int Page { get; } = page;
+
+    public int PageSize { get; } = pageSize;
 }
 
 internal class GetBudgetListRequestHandler(
@@ -16,9 +21,9 @@ internal class GetBudgetListRequestHandler(
     ICompanyRepository companyRepository,
     ICustomerRepository customerRepository,
     IMapper mapper)
-    : IRequestHandler<GetBudgetListRequest, List<GetBudgetListEntryDto>>
+    : IRequestHandler<GetBudgetListRequest, PageDto<GetBudgetListEntryDto>>
 {
-    public async Task<List<GetBudgetListEntryDto>> Handle(GetBudgetListRequest request, CancellationToken cancellationToken)
+    public async Task<PageDto<GetBudgetListEntryDto>> Handle(GetBudgetListRequest request, CancellationToken cancellationToken)
     {
         var budgets = await budgetRepository.GetBudgetListAsync(request.CompanyId);
         var company = await companyRepository.GetCompanyByIdAsync(request.CompanyId);
@@ -31,6 +36,9 @@ internal class GetBudgetListRequestHandler(
         result.AddRange(customers.Select(o => new GetBudgetListEntryDto { CustomerId = o.Id, CustomerName = $"{o.LastName}, {o.FirstName}".Trim() }));
         var pricePerHour = company!.PricePerHour == 0 ? 1 : company.PricePerHour;
         result.ForEach(o => o.RemainingHours = o.TotalAmount / pricePerHour);
-        return result.OrderBy(o => o.CustomerName).ToList();
+        return new(
+            result.OrderBy(o => o.CustomerName).Skip((request.Page - 1) * request.PageSize).Take(request.PageSize).ToList(),
+            request.Page,
+            (int)Math.Ceiling((double)result.Count / request.PageSize));
     }
 }
